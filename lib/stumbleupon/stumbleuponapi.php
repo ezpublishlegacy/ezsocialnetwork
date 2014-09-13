04c13c9a99f78827739a4e1664331e92c0de917e
Dany Ralantonisainana <lendormi1984@gmail.com>
2014-09-14 01:15:43 +0200

    stumbleupon

--- lib/stumbleupon/stumbleuponapi.php
+++ lib/stumbleupon/stumbleuponapi.php
@@ -28,14 +28,11 @@ class StumbleUponAPI extends SocialRequest
     }
 
     /**
-     * If, for security reasons, you can not load the LinkedIn JavaScript framework 
-     * you can make a direct request to the share count endpoint to retrieve the count.
-     * Using the endpoint is as simple as sending the URL of interest and receiving 
-     * back a small piece of JSON for you to process.
-     * This endpoint is intended primarily for LinkedIn internal use and as such there 
-     * is no guarantee about the stability of the URL or the permanence of its signature.
+     * The Badge API allows web site publishers to create custom StumbleUpon badges
+     * on their web pages by querying information about the page which the badge 
+     * sits on. This is provided through a simple REST API with output in JSON
+     * format.
      * 
-     * Check back here for future changes.
      * @param  string $url [description]
      * @return [type]      [description]
      */
@@ -43,13 +40,42 @@ class StumbleUponAPI extends SocialRequest
         $instanceStumbleUpon = new StumbleUponAPI();
         /**
          * This instance return an object JSON
-         * @var count
-         * @var fCnt
-         * @var fCntPlusOne
-         * @var url
+         * @var result
+         * result           object      The resulting data from the API call.
+         *      url         string      The URL you sent us. This may be a canonicalized URL,
+         *                              but should point to the same content. A canonical URL
+         *                              in our system is one without tracking codes, anchor #’s, 
+         *                              etc.
+         *      in_index    literal     If the URL you sent has been submitted to us by our users,
+         *                              this will return true. Otherwise, we will return false.
+         *      publicid    string      This is the identifier to this URL in our index. This 
+         *                              value is unused for now, but will very likely be used in
+         *                              the future.
+         *      views       number      The number of times this URL has been stumbled upon by our users.
+         *      title       string      The title of the page as deduced from the <title>tag of the URL.
+         *      thumbnail   string      The URL to the regular sized thumbnail image (129 x 86 pixels).
+         *      thumbnail_b string      The URL to the big thumbnail image (283 x 184 pixels).
+         *      submit_link string      The URL to navigate users to who want to submit the URL to
+         *                              us if new, and rate the URL if existing. This is a page designed
+         *                              to layout in a full page, not a pop-up.
+         *      badge_link  string      The URL to navigate userse to who want to submit or rate the URL.
+         *                              This is a page designed to layout in a pop-up with height and
+         *                              width ratio of 5 to 4 (so 450 x 360 pixels would be an appropriate
+         *                              size). The actual pixel size will depend on the end-user’s graphics
+         *                              capabilities and default font size.
+         *      info_link   string      The URL to display an informational page about a URL in our index.
+         * timestamp        number      Date/time when the response was served (in Unix time format).
+         * success          literal     true if no error was encountered in fetching and returning a
+         *                              response. false otherwise (see below for possible error codes).
          */
         $jsonString = $instanceStumbleUpon->request($instanceStumbleUpon->url("services/1.01/badge.getinfo"), $parameters);
         $json = json_decode($jsonString, true);
+        /**
+         * 9000     Service temporarily unavailable
+         * 9020     Bad or missing parameter: url
+         * 9030     Rate-limit exceeded
+         * 9031     Rate-limit exceeded
+         */
         if (isset($json['error_code'])) {
             eZDebug::writeError( $json['error_message'] . ": ". $json['error_message'], 'StumbleUpon API' );
             return false;
